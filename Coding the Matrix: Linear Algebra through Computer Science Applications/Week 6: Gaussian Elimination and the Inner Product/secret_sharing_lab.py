736ff2ec0b58c63145de6a6d9c6225e52fe52809
nineinch <serg.dfc@yandex.ru>
2015-03-16 21:17:25 +0000

    Secret sharing lab done

--- Coding the Matrix: Linear Algebra through Computer Science Applications/Week 6: Gaussian Elimination and the Inner Product/secret_sharing_lab.py
+++ Coding the Matrix: Linear Algebra through Computer Science Applications/Week 6: Gaussian Elimination and the Inner Product/secret_sharing_lab.py
@@ -5,30 +5,33 @@ coursera = 1
 import random
 from GF2 import one
 from vecutil import list2vec
-
-
+from vec import Vec
 
 ## 1: (Task 1) Choosing a Secret Vector
 def randGF2(): return random.randint(0,1)*one
+
+def randVector(n): return list2vec([randGF2(), randGF2(), randGF2(), randGF2(), randGF2(), randGF2()])
+
 
 a0 = list2vec([one, one,   0, one,   0, one])
 b0 = list2vec([one, one,   0,   0,   0, one])
 
 def choose_secret_vector(s,t):
-    pass
-
+    u = list2vec([randGF2() for i in range(len(a0.D))])
+    while (a0*u != s or b0*u != t):
+        u = list2vec([randGF2() for i in range(len(a0.D))])
+    return u
 
 
 ## 2: (Task 2) Finding Secret Sharing Vectors
 # Give each vector as a Vec instance
-secret_a0 = ...
-secret_b0 = ...
-secret_a1 = ...
-secret_b1 = ...
-secret_a2 = ...
-secret_b2 = ...
-secret_a3 = ...
-secret_b3 = ...
-secret_a4 = ...
-secret_b4 = ...
-
+secret_a0 = a0
+secret_b0 = b0
+secret_a1 = list2vec([0,   one, 0,   one, 0,   one])
+secret_b1 = list2vec([0,   one, 0,   0,   0,   0])
+secret_a2 = list2vec([one, one, 0,   0,   one, one])
+secret_b2 = list2vec([0,   one, one, one, one, one])
+secret_a3 = list2vec([one, 0,   0,   one, one, one])
+secret_b3 = list2vec([one, one, one, 0,   one, one])
+secret_a4 = list2vec([0,   one, one, one, one, 0])
+secret_b4 = list2vec([0,   0,   one, one, 0,   one])
