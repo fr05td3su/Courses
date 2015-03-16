931fdef9e4158924aa19b7a1249b719dc00eea98
nineinch <serg.dfc@yandex.ru>
2015-03-16 20:43:33 +0000

    GE hw done

--- Coding the Matrix: Linear Algebra through Computer Science Applications/Week 6: Gaussian Elimination and the Inner Product/Gaussian_Elimination_problems.py
+++ Coding the Matrix: Linear Algebra through Computer Science Applications/Week 6: Gaussian Elimination and the Inner Product/Gaussian_Elimination_problems.py
@@ -3,34 +3,28 @@ coursera = 1
 # Please fill out this stencil and submit using the provided submission script.
 
 from matutil import *
+from vecutil import zero_vec
 from GF2 import one
-
-
 
 ## 1: (Problem 1) Recognizing Echelon Form
 # Write each matrix as a list of row lists
 
-echelon_form_1 = [[   ...   ],
-                  [   ...   ],
-                  [   ...   ],
-                  [   ...   ],
-                  [   ...   ]]
-
-echelon_form_2 = [[   ...   ],
-                  [   ...   ],
-                  [   ...   ],
-                  [   ...   ]]
-
-echelon_form_3 = [[   ...   ],
-                  [   ...   ],
-                  [   ...   ]]
-
-echelon_form_4 = [[   ...   ],
-                  [   ...   ],
-                  [   ...   ],
-                  [   ...   ]]
-
-
+echelon_form_1 = [[1,2,0,2,0],
+                  [0,1,0,3,4],
+                  [0,0,2,3,4],
+                  [0,0,0,2,0],
+                  [0,0,0,0,4]]
+echelon_form_2 = [[0,4,3,4,4],
+                  [0,0,4,2,0],
+                  [0,0,0,0,1],
+                  [0,0,0,0,0]]
+echelon_form_3 = [[1,0,0,1],
+                  [0,0,0,1],
+                  [0,0,0,0]]
+echelon_form_4 = [[1,0,0,0],
+                  [0,1,0,0],
+                  [0,0,0,0],
+                  [0,0,0,0]]
 
 ## 2: (Problem 2) Is it echelon?
 def is_echelon(A):
@@ -56,25 +50,34 @@ def is_echelon(A):
         >>> is_echelon([[0],[1]])
         False
     '''
-    pass
-
-
+    last_pivot = -1
+    for i in range(len(A)):
+        pivot = last_pivot
+        for j in range(len(A[i])):
+            if A[i][j] != 0:
+                pivot = j
+                if pivot <= last_pivot:
+                    return False
+                else:
+                    break
+            else:
+                pivot += 1
+        last_pivot = pivot
+    return True
 
 ## 3: (Problem 3) Solving with Echelon Form: No Zero Rows
 # Give each answer as a list
 
-echelon_form_vec_a = ...
-echelon_form_vec_b = ...
-echelon_form_vec_c = ...
-
-
+echelon_form_vec_a = [1,0,3,0]
+echelon_form_vec_b = [-3,0,-2,3]
+echelon_form_vec_c = [-5,0,2,0,2]
 
 ## 4: (Problem 4) Solving with Echelon Form
 # If a solution exists, give it as a list vector.
 # If no solution exists, provide "None" (without the quotes).
 
-solving_with_echelon_form_a = ...
-solving_with_echelon_form_b = ...
+solving_with_echelon_form_a = None
+solving_with_echelon_form_b = [21,0,2,0,0]
 
 
 
@@ -95,22 +98,23 @@ def echelon_solve(row_list, label_list, b):
     >>> echelon_solve(U_rows, cols, b_list) == Vec({'B', 'C', 'A', 'D', 'E'},{'B': 0, 'C': one, 'A': one})
     True
     '''
-    pass
-
-
+    x = Vec(set(label_list),{})
+    for i in range(len(row_list)-1,-1,-1):
+        for key in label_list:
+            if row_list[i][key] != 0:
+                x[key] = b[i] - row_list[i]*x
+                break
+    return x
 
 ## 6: (Problem 6) Solving General Matrices via Echelon
-row_list = [ ... ]    # Provide as a list of Vec instances
-label_list = [ ... ] # Provide as a list
-b = [ ... ]          # Provide as a list of GF(2) values
-
-
+row_list = [Vec({'A', 'C', 'B', 'D'},{'A': one, 'C': 0, 'B': one, 'D': one}), Vec({'A', 'C', 'B', 'D'},{'A': 0, 'C': 0, 'B': one, 'D': 0}), Vec({'A', 'C', 'B', 'D'},{'A': 0, 'C': one, 'B': 0, 'D': 0}), Vec({'A', 'C', 'B', 'D'},{'A': 0, 'C': 0, 'B': 0, 'D': one})]
+# Provide as a list of Vec instances
+label_list = ['A', 'B', 'C', 'D'] # Provide as a list
+b = [one,one,0,0] # Provide as a list
 
 ## 7: (Problem 7) Nullspace A
-null_space_rows_a = {...} # Put the row numbers of M from the PDF
-
-
+null_space_rows_a = {3,4} # Put the row numbers of M from the PDF
 
 ## 8: (Problem 8) Nullspace B
-null_space_rows_b = {...} # Put the row numbers of M from the PDF
+null_space_rows_b = {4} # Put the row numbers of M from the PDF
 
